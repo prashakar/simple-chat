9debce5ca36f64f9d19872ff01bd130561001130
Prashakar Laptop <prashakar.prabagaran@uoit.net>
2016-09-27 16:37:08 -0400

    some changes

--- client.py
+++ client.py
@@ -4,11 +4,12 @@ import socket
 tcp_ip = "127.0.0.1"
 tcp_port = 5005
 buffer_size = 1024
-message = "hello this is a test!"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((tcp_ip,tcp_port))
-s.send(message)
-data = s.recv(buffer_size)
+while 1:
+	message = raw_input("Enter message to send:\n")	
+	s.send(message)
+	data = s.recv(buffer_size)
 s.close()
 print "received data: ",data
