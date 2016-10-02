80d4b53e1291b8d8073f221289cc60e37f497786
Prashakar PC <prashakar.prabagaran@uoit.net>
2016-10-02 05:33:18 -0400

    multiple clients can now connect

--- new_server.py
+++ new_server.py
@@ -7,19 +7,31 @@ class Connections(object):
 	The run() method will be started and run in background until the application exits
 	"""
 	
-	def __init__(self,interval=1):
+	def __init__(self,interval=0.1):
 		self.interval = interval
 
 		thread = threading.Thread(target=self.run)
 		thread.daemon = True
 		thread.start()
+	
+	def user_run(self,conn,addr):
+		while True:
+			data = conn.recv(buff_size)
+			if data:
+ 	                       print ("<" + str(conn.getpeername()) + "> " + data)
+			time.sleep(self.interval)
 
 	def run(self):
 		while True:
-			print "Listening for client connections"
+			print "\nListening for client connections"
 			conn, addr = sock.accept()	
 			print "Client connected: (%s,%s)" % addr
-			client_conn.append([conn,addr])
+			client_conn.append([conn,addr[0],addr[1]])
+			
+			user_thread = threading.Thread(target=self.user_run, args=(conn,addr))
+			user_thread.daemon = True
+			user_thread.start()
+			
 			time.sleep(self.interval)
 			
 if __name__ == "__main__":
@@ -37,9 +49,5 @@ if __name__ == "__main__":
 	listen_conn = Connections()
 	
 	while 1:
-		for client in client_conn:
-			print "\nTOTAL Clients connected: %s" % client_conn
-			data = client[0].recv(buff_size)
-			if data:
-				print ("<" + str(client[0].getpeername()) + "> " + data)
+		pass
 	sock.close()
